c91add49ca9cb9ae773b9c5d4ec9c32ac7c23bfe
Velcis Ribeiro <velcisribeirodj@gmail.com>
2022-10-05 10:01:41 -0300

    updated to pt_br

--- src/CardView.js
+++ src/CardView.js
@@ -110,56 +110,122 @@ export default class CardView extends Component {
   };
 
   render() {
-    const { focused,
-      brand, name, number, expiry, cvc, customIcons,
-      placeholder, imageFront, imageBack, scale, fontFamily } = this.props;
+    const {
+      focused,
+      brand,
+      name,
+      number,
+      expiry,
+      cvc,
+      customIcons,
+      placeholder,
+      imageFront,
+      imageBack,
+      scale,
+      fontFamily,
+    } = this.props;
 
     const Icons = { ...defaultIcons, ...customIcons };
     const isAmex = brand === "american-express";
     const shouldFlip = !isAmex && focused === "cvc";
 
     const containerSize = { ...BASE_SIZE, height: BASE_SIZE.height * scale };
-    const transform = { transform: [
-      { scale },
-      { translateY: ((BASE_SIZE.height * (scale - 1) / 2)) },
-    ] };
+    const transform = {
+      transform: [
+        { scale },
+        { translateY: (BASE_SIZE.height * (scale - 1)) / 2 },
+      ],
+    };
 
     return (
       <View style={[s.cardContainer, containerSize]}>
-        <FlipCard style={{ borderWidth: 0 }}
+        <FlipCard
+          style={{ borderWidth: 0 }}
           flipHorizontal
           flipVertical={false}
           friction={10}
           perspective={2000}
           clickable={false}
-          flip={shouldFlip}>
-          <ImageBackground style={[BASE_SIZE, s.cardFace, transform]}
-            source={imageFront}>
-              <Image style={[s.icon]}
-                source={Icons[brand]} />
-              <Text style={[s.baseText, { fontFamily }, s.number, !number && s.placeholder, focused === "number" && s.focused]}>
-                { !number ? placeholder.number : number }
+          flip={shouldFlip}
+        >
+          <ImageBackground
+            style={[BASE_SIZE, s.cardFace, transform]}
+            source={imageFront}
+          >
+            <Image style={[s.icon]} source={Icons[brand]} />
+            <Text
+              style={[
+                s.baseText,
+                { fontFamily },
+                s.number,
+                !number && s.placeholder,
+                focused === "number" && s.focused,
+              ]}
+            >
+              {!number ? placeholder.number : number}
+            </Text>
+            <Text
+              style={[
+                s.baseText,
+                { fontFamily },
+                s.name,
+                !name && s.placeholder,
+                focused === "name" && s.focused,
+              ]}
+              numberOfLines={1}
+            >
+              {!name ? placeholder.name : name.toUpperCase()}
+            </Text>
+            <Text
+              style={[
+                s.baseText,
+                { fontFamily },
+                s.expiryLabel,
+                s.placeholder,
+                focused === "expiry" && s.focused,
+              ]}
+            >
+              MÊS/ANO
+            </Text>
+            <Text
+              style={[
+                s.baseText,
+                { fontFamily },
+                s.expiry,
+                !expiry && s.placeholder,
+                focused === "expiry" && s.focused,
+              ]}
+            >
+              {!expiry ? placeholder.expiry : expiry}
+            </Text>
+            {isAmex && (
+              <Text
+                style={[
+                  s.baseText,
+                  { fontFamily },
+                  s.amexCVC,
+                  !cvc && s.placeholder,
+                  focused === "cvc" && s.focused,
+                ]}
+              >
+                {!cvc ? placeholder.cvc : cvc}
               </Text>
-              <Text style={[s.baseText, { fontFamily }, s.name, !name && s.placeholder, focused === "name" && s.focused]}
-                numberOfLines={1}>
-                { !name ? placeholder.name : name.toUpperCase() }
-              </Text>
-              <Text style={[s.baseText, { fontFamily }, s.expiryLabel, s.placeholder, focused === "expiry" && s.focused]}>
-                MONTH/YEAR
-              </Text>
-              <Text style={[s.baseText, { fontFamily }, s.expiry, !expiry && s.placeholder, focused === "expiry" && s.focused]}>
-                { !expiry ? placeholder.expiry : expiry }
-              </Text>
-              { isAmex &&
-                  <Text style={[s.baseText, { fontFamily }, s.amexCVC, !cvc && s.placeholder, focused === "cvc" && s.focused]}>
-                    { !cvc ? placeholder.cvc : cvc }
-                  </Text> }
+            )}
           </ImageBackground>
-          <ImageBackground style={[BASE_SIZE, s.cardFace, transform]}
-            source={imageBack}>
-              <Text style={[s.baseText, s.cvc, !cvc && s.placeholder, focused === "cvc" && s.focused]}>
-                { !cvc ? placeholder.cvc : cvc }
-              </Text>
+          <ImageBackground
+            style={[BASE_SIZE, s.cardFace, transform]}
+            source={imageBack}
+          >
+            <Text
+              style={[
+                s.baseText,
+                s.cvc,
+                !cvc && s.placeholder,
+                focused === "cvc" && s.focused,
+              ]}
+            >
+              {!cvc ? placeholder.cvc : cvc}
+            </Text>
           </ImageBackground>
         </FlipCard>
       </View>
